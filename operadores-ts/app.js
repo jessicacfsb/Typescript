7ad2b0d59feada194a7752bd8aa8d415b3f03a05
jessicacfsb <jessica.cfsb@gmail.com>
2021-11-03 18:59:26 -0300

    operadores Typescript

---
+++ operadores-ts/app.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    var pi;
+    pi = 3.1415;
+    alert("The pi number is " + pi);
+};
---
+++ operadores-ts/app.ts
@@ -0,0 +1,7 @@
+window.onload = () => {
+    
+    var pi: number;
+    pi = 3.1415;
+
+    alert("The pi number is " + pi)
+};
